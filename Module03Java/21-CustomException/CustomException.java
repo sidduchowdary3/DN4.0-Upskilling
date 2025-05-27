614a767d199f05433f62b756c428f8e2210cc918
sidduchowdary3 <164843613+sidduchowdary3@users.noreply.github.com>
2025-05-27 21:23:28 +0530

    Update CustomException.java

--- Module03Java/21-CustomException/CustomException.java
+++ Module03Java/21-CustomException/CustomException.java
@@ -1,14 +1,10 @@
 import java.util.Scanner;
-
-// Custom exception class
 class InvalidAgeException extends Exception {
     public InvalidAgeException(String message) {
         super(message);
     }
 }
-
 public class CustomException {
-    // Method to check age
     public static void checkAge(int age) throws InvalidAgeException {
         if (age < 18) {
             throw new InvalidAgeException("Access denied. Age must be 18 or above.");
@@ -16,20 +12,15 @@ public class CustomException {
             System.out.println("Access granted. You are eligible.");
         }
     }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Prompt user for age
         System.out.print("Enter your age: ");
         int age = scanner.nextInt();
-
         try {
             checkAge(age);
         } catch (InvalidAgeException e) {
             System.out.println(e.getMessage());
         }
-
         scanner.close();
     }
 }
