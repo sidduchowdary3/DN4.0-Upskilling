3d8dd4777da73434e1eac72e0752300b643c952e
sidduchowdary3 <164843613+sidduchowdary3@users.noreply.github.com>
2025-05-27 21:16:07 +0530

    Update Inheritance.java

--- Module03Java/18-Inheritance/Inheritance.java
+++ Module03Java/18-Inheritance/Inheritance.java
@@ -1,20 +1,15 @@
 public class Inheritance {
-
-    // Base class
     static class Animal {
         void makeSound() {
             System.out.println("Animal makes a sound");
         }
     }
-
-    // Subclass
     static class Dog extends Animal {
         @Override
         void makeSound() {
             System.out.println("Bark");
         }
     }
-
     public static void main(String[] args) {
         // Create an instance of Animal
         Animal genericAnimal = new Animal();
