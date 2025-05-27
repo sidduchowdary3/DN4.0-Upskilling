2bacda10714568615479ca9b451e643ae46d2101
sidduchowdary3 <164843613+sidduchowdary3@users.noreply.github.com>
2025-05-27 21:09:02 +0530

    Update Fibonacci.java

--- Module03Java/13-Fibonocci/Fibonacci.java
+++ Module03Java/13-Fibonocci/Fibonacci.java
@@ -1,8 +1,6 @@
 import java.util.Scanner;
 
 public class Fibonacci {
-
-    // Recursive method to calculate the nth Fibonacci number
     public static int fibonacci(int n) {
         if (n == 0 || n == 1) {
             return n;
@@ -10,14 +8,10 @@ public class Fibonacci {
             return fibonacci(n - 1) + fibonacci(n - 2);
         }
     }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Prompt user for input
         System.out.print("Enter a positive integer (n): ");
         int n = scanner.nextInt();
-
         if (n < 0) {
             System.out.println("Invalid input. Please enter a positive integer.");
         } else {
